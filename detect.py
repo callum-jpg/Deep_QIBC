b9a03d80fb68caf89170d240c1be8c20e736c418
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2021-01-29 17:09:25 +0000

    options and time. commented out tests

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import skimage
+import time
 
 mrcnn_dir = os.path.abspath("Mask_RCNN")
 sys.path.append(mrcnn_dir)
@@ -39,38 +40,56 @@ class AdjustNucleusConfigLow(nucleus.NucleusConfig):
         IMAGE_MIN_DIM = 512
         IMAGE_MAX_DIM = 512
         
-class AdjustNucleusConfigMed(nucleus.NucleusInferenceConfig):
+class AdjustNucleusConfigMed(nucleus.NucleusConfig):
         GPU_COUNT = 1
         IMAGES_PER_GPU = 1
         IMAGE_RESIZE_MODE = "square"
         IMAGE_MIN_DIM = 1024
-        IMAGE_MAX_DIM = 1024
+        IMAGE_MAX_DIM = 1024       
         
-        
-class AdjustNucleusConfigHigh(nucleus.NucleusInferenceConfig):
+class AdjustNucleusConfigHigh(nucleus.NucleusConfig):
         GPU_COUNT = 1
         IMAGES_PER_GPU = 1
+        # Min max not available with pad64
         IMAGE_RESIZE_MODE = "pad64"
+        IMAGE_MIN_SCALE = 2
 
 
 class DetectNucleus:
     def __init__(self):
         # Instantiate the desired computation config
-        self.empty = []
         self.config_low = AdjustNucleusConfigLow()
         self.config_med = AdjustNucleusConfigMed()
         self.config_high = AdjustNucleusConfigHigh()
+        self.results = []
         
-    def run_detection(self, computation_requirement=None, device=None):
+    def run_detection(self, images, object_channel, 
+                      computation_requirement=None, device=None):
+        """
+        Runs the Mask R-CNN model detection. Images is from images.image_info.
+        
+        object_channel: the image that the masks will be generated 
+        for (eg. DAPI-stained nuclei).
+        
+        computation_requirement: low/med/high will determine the intensity 
+        that the model is run at. Low settings are less accurate but less
+        computationally intensive than high settings.
+        
+        device: select cpu or gpu for running the model
+        """
         
         # Load optional configs
         if computation_requirement == "low":
+            print("Using low settings")
             config = self.config_low
         if computation_requirement == "med":
+            print("Using medium settings")
             config = self.config_med
         if computation_requirement == "high":
+            print("Using high settings")
             config = self.config_high
         else:
+            print("Using low settings")
             config = self.config_low
 
         # Load preferred device
@@ -81,6 +100,7 @@ class DetectNucleus:
         else:
             DEVICE = "/cpu:0"
             
+            
         with tf.device(DEVICE): 
                 model = modellib.MaskRCNN(mode="inference",
                               model_dir=LOGS_DIR,
@@ -90,181 +110,64 @@ class DetectNucleus:
         print("Loading weights ", NUCLEUS_TRAINED_WEIGHTS)
         model.load_weights(NUCLEUS_TRAINED_WEIGHTS, by_name=True)
         
-        # Load images
-        # NEED TO ADD OPTIONS FOR GIVING DIRECTORY/CHANNELS
-        images = load_images.LoadImages()
+        start_time = time.time()
         
-        images.add_channels(["w1DAPI", "w2Cy5", "w3mCherry", "w4GFP"])
-        
-        image_dir = "images"
-        
-        images.load_images(image_dir)
-        
-        results = [] 
-        for img in images.image_info:
-            print(img['w1DAPI']) # returns the image name
-            # img_rgb = skimage.color.gray2rgb(img['w1DAPI'][1])
-            # results.append([img['w1DAPI'][0], model.detect([img['w1DAPI'][1]], verbose=1)])
-            results.append([img['w1DAPI'][0], model.detect([img['w1DAPI'][1]], verbose=1)])
-        
-        return results
-        
-        
-  
+        for img in images:
+            print(img[object_channel]) # returns the image name
+            self.results.append([img[object_channel][0], 
+                                 model.detect([img[object_channel][1]], verbose=1)])
             
-            
-            
-
-        
-#%%
-
-DetectNucleus().run_detection()
-
-
-#%% testing
-
-DEVICE = "/cpu:0" 
-
-config = DetectNucleus().config_low
-
-# Test class varaibles with DetectNucleus().config_low.IMAGE_RESIZE_MODE
-# or config.IMAGE_RESIZE_MODE
-
-# Create model in inference mode
-with tf.device(DEVICE):
-    model = modellib.MaskRCNN(mode="inference",
-                              model_dir=LOGS_DIR,
-                              config=config)
-
-
-#%% Will it blend?
-import skimage
-from mrcnn import visualize
-
-
-DEVICE = "/cpu:0" 
-
-
-# Inference Configuration
-config = AdjustNucleusConfigLow()
-
-#config = nucleus.NucleusInferenceConfig()
-
-# Create model in inference mode
-with tf.device(DEVICE):
-    model = modellib.MaskRCNN(mode="inference",
-                              model_dir=LOGS_DIR,
-                              config=config)
-    
-# Load weights
-print("Loading weights ", NUCLEUS_TRAINED_WEIGHTS)
-model.load_weights(NUCLEUS_TRAINED_WEIGHTS, by_name=True)
-
-# results = [] 
-# for img in images.image_info:
-#     print(img['w1DAPI']) # returns the image name
-#     # img_rgb = skimage.color.gray2rgb(img['w1DAPI'][1])
-#     results.append([img['w1DAPI'][0], model.detect([
-#         skimage.color.gray2rgb(img['w1DAPI'][1])], verbose=1)])
-
-images = load_images.LoadImages()
-
-images.add_channels(["w1DAPI", "w2Cy5", "w3mCherry", "w4GFP"])
-
-image_dir = "images"
-
-images.load_images(image_dir)
-
-
-results = [] 
-for img in images.image_info:
-    print(img['w1DAPI']) # returns the image name
-    # img_rgb = skimage.color.gray2rgb(img['w1DAPI'][1])
-    # results.append([img['w1DAPI'][0], model.detect([img['w1DAPI'][1]], verbose=1)])
-    results.append([img['w1DAPI'][0], model.detect([img['w1DAPI'][1]], verbose=1)])
-    
-res1 = results[0][1][0]
-    
-# In img_array, 0th element, 1st element is the image array
-#display_image = skimage.color.gray2rgb(images.image_info[0]['w1DAPI'][1])
-
-class_names = ['','']
-
-visualize.display_instances(images.image_info[0]['w1DAPI'][1], 
-                            res1['rois'], res1['masks'], res1['class_ids'], class_names,
-                            show_bbox=False)
-
+        print("--- {} seconds ---".format(time.time() - start_time))
 
     
-# plt.imshow(skimage.color.rgb2gray(images.image_info[0]['w1DAPI'][1]), cmap="gray")
-gray_image = skimage.color.rgb2gray(images.image_info[0]['w1DAPI'][1])
-plt.imshow(gray_image, cmap="gray")
-plt.axis("off")
 
+# #%%
 
+# # Load images
+# images = load_images.LoadImages()
 
+# images.add_channels(["w1DAPI", "w2Cy5", "w3mCherry", "w4GFP"])
 
-#%%
-for i in images.image_info:
-    print(i['w1DAPI'][0], i['w1DAPI'][1])
+# image_dir = "images"
 
+# images.load_images(image_dir)
 
-#%%
+# # Run detection
+# nuclei_detection = DetectNucleus()
 
-import nucleus1 # Contains NucleusDataset1
+# # Select channel to run detection on (in this case, DAPI)
+# object_channel = images.channels[0]
 
-DEVICE = "/cpu:0" 
+# nuclei_detection.run_detection(images.image_info, object_channel, "low", "cpu")
 
-# Inference Configuration
-# config = nucleus.NucleusInferenceConfig()
+# nuclei_detection.results
 
-config = AdjustNucleusConfigLow()
+# #%%
 
-# Create model in inference mode
-with tf.device(DEVICE):
-    model = modellib.MaskRCNN(mode="inference",
-                              model_dir=LOGS_DIR,
-                              config=config)
+# from mrcnn import visualize
+
+# # images = load_images.LoadImages()
+
+# # images.add_channels(["w1DAPI", "w2Cy5", "w3mCherry", "w4GFP"])
+
+# # image_dir = "images"
+
+# # images.load_images(image_dir)
+
+# res1 = nuclei_detection.results[0][1][0]
     
-# Load weights
-print("Loading weights ", NUCLEUS_TRAINED_WEIGHTS)
-model.load_weights(NUCLEUS_TRAINED_WEIGHTS, by_name=True)
-
-import time
-start_time = time.time()
-
-data_dir = os.path.join("datasets/nucleus/lil-test")
-
-data = nucleus1.NucleusDataset1()
-img_array = data.load_nucleus(data_dir)
-# img_arr is a list of tuples. [0] is the image name, [1] is the cv2 image array
-
-# Below iterates through the image array and adds the image name and 
-# model.detect output to a list
-results = [] 
-for img in img_array:
-    print(img) # returns the image name
-    results.append([img[0], model.detect([img[1]], verbose=1)])
-    
-print("--- {} seconds ---".format(time.time() - start_time))
-
-
-res = results[0][1][0]
-
-class_names = ['BG','nucleus']
-
 # # In img_array, 0th element, 1st element is the image array
-visualize.display_instances(img_array[0][1], res['rois'], res['masks'], res['class_ids'], class_names, res['scores'])
+# #display_image = skimage.color.gray2rgb(images.image_info[0]['w1DAPI'][1])
 
-plt.imshow(cv2.cvtColor(img_array[0][1], cv2.COLOR_RGB2GRAY), cmap="gray")
-plt.axis("off")
+# class_names = ['','']
+
+# visualize.display_instances(images.image_info[0]['w1DAPI'][1], 
+#                             res1['rois'], res1['masks'], res1['class_ids'], class_names,
+#                             show_bbox=False)
 
 
-#%% Investigating image shape
-import skimage
-
-img_array[0][1].shape
-
-skimage.color.gray2rgb(images.image_info[0]['w1DAPI'][1]).shape
-        
-        
+    
+# # plt.imshow(skimage.color.rgb2gray(images.image_info[0]['w1DAPI'][1]), cmap="gray")
+# gray_image = skimage.color.rgb2gray(images.image_info[0]['w1DAPI'][1])
+# plt.imshow(gray_image, cmap="gray")
+# plt.axis("off")
