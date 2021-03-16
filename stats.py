d114ae31b33cfac82acb715847433024e9924661
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2021-03-16 21:48:12 +0000

    removed old code and added plotting. Will move plotting code somewhere else later

--- stats.py
+++ stats.py
@@ -10,6 +10,7 @@ import numpy as np
 import os
 import sys
 import matplotlib.pyplot as plt
+import time
 
 mrcnn_dir = os.path.abspath("Mask_RCNN")
 sys.path.append(mrcnn_dir)
@@ -23,312 +24,9 @@ import cv2
 
 from detect import NUCLEUS_TRAINED_WEIGHTS, AdjustNucleusConfigLow, AdjustNucleusConfigMed, AdjustNucleusConfigHigh, LOGS_DIR
 import load_images
+import visualise
 
-#%%
 
-class AdjustNucleusConfigHigh1(Config):
-    """Configuration for training on the nucleus segmentation dataset.
-    Edited from Waleed's nucleus implementation of mask RCNN"""
-    # Give the configuration a recognizable name
-    NAME = "nucleus"
-
-    # Adjust depending on your GPU memory
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-    # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + nucleus
-
-    # Don't exclude based on confidence. Since we have two classes
-    # then 0.5 is the minimum anyway as it picks between nucleus and BG
-    DETECTION_MIN_CONFIDENCE = 0
-
-    # Backbone network architecture
-    # Supported values are: resnet50, resnet101
-    BACKBONE = "resnet50"
-
-    # Input image resizing
-    # Random crops of size 512x512
-    IMAGE_RESIZE_MODE = "square"
-    IMAGE_MIN_DIM = 1024
-    IMAGE_MAX_DIM = 1024
-    IMAGE_MIN_SCALE = 2.0
-
-    # Length of square anchor side in pixels
-    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)
-
-    # ROIs kept after non-maximum supression (training and inference)
-    POST_NMS_ROIS_TRAINING = 1000
-    POST_NMS_ROIS_INFERENCE = 2000
-
-    # Non-max suppression threshold to filter RPN proposals.
-    # You can increase this during training to generate more propsals.
-    RPN_NMS_THRESHOLD = 0.9
-
-    # How many anchors per image to use for RPN training
-    RPN_TRAIN_ANCHORS_PER_IMAGE = 64
-
-    # Image mean (RGB)
-    MEAN_PIXEL = np.array([43.53, 39.56, 48.22])
-
-    # If enabled, resizes instance masks to a smaller size to reduce
-    # memory load. Recommended when using high-resolution images.
-    USE_MINI_MASK = True
-    MINI_MASK_SHAPE = (56, 56)  # (height, width) of the mini-mask
-
-    # Number of ROIs per image to feed to classifier/mask heads
-    # The Mask RCNN paper uses 512 but often the RPN doesn't generate
-    # enough positive proposals to fill this and keep a positive:negative
-    # ratio of 1:3. You can increase the number of proposals by adjusting
-    # the RPN NMS threshold.
-    TRAIN_ROIS_PER_IMAGE = 128
-
-    # Maximum number of ground truth instances to use in one image
-    MAX_GT_INSTANCES = 200
-
-    # Max number of final detections per image
-    DETECTION_MAX_INSTANCES = 400
-
-
-config = AdjustNucleusConfigHigh1()
-#config = qibc_nucleus.NucleusInferenceConfig()
-
-#%
-# img_dir = 'datasets/nucleus/bw_train/'
-img_dir = 'datasets/nucleus/label_test/'
-
-dataset = qibc_nucleus.NucleiDataset()
-dataset.load_nuclei(img_dir) 
-dataset.prepare()
-
-# image_id = random.choice(dataset.image_ids)
-image_id = 1
-
-# You give the dataset object to load_image_gt. It is the load_image_gt
-# function which then runs dataset.load_mask to load all available masks
-# as defined by your load_masks function in qibc_nucleus.
-# These gt masks are later matched with the inference masks and scores
-# calculated
-image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-    modellib.load_image_gt(dataset, config, image_id, use_mini_mask=False)
-    
-# Convert to 8-bit
-# Since model was trained on 8-bit DSB18 images but metamorph are 16-bit,
-# convert to 8-bit to improve score
-out = np.zeros(image.shape, dtype=np.uint8)
-
-image = cv2.normalize(image, out, 0, 255, cv2.NORM_MINMAX)
-    
-info = dataset.image_info[image_id]
-
-
-# This doesn't seem to work for high resolution images
-# This is because load_image_gt resizes images and requries the input 
-# dimensions to be multiples of 64. The resize dimensions is declared by 
-# the config
-# Thus, utils.resize_image is scaling the gt image down to 512x512
-# and then running detection on this smaller image. 
-# Moreover, the molded images are normalised (in addition to resized) for 
-# input into the neural network
-# GT masks load fine, but prediction isn't accurate. 
-# Moreover, the image loads weird too - perhaps due to RGB conversions
-
-# The above statements are false. Mistakes in resizing were due to "crop"
-# IMAGE_RESIZE_MODE being selected. 
-# The GT image 'looking weird' was due to shortened histogram. Consider
-# playing around with skimage.io stuff
-# 
-# However, this method leads to no object detection for some reason - why?
-# Testing with the DSB18 images works fine, though these were used to train
-# so probably not a good idea to use at all for this.
-# UPDATE: it seems to be something to do with image resolution. Perhaps 
-# due to the training taking place on 512x512 images, when a larger image, 
-# eg metamorph 1040x1392
-
-
-
-
-#% Exploring dataset
-
-mask, class_ids = dataset.load_mask(image_id)
-
-dataset.load_mask
-
-test = dataset.load_image(image_id)
-
-#%  Load model
-
-LOGS_DIR = os.path.join("logs")
-
-weights = 'weights/mask_rcnn_nucleus.h5'
-
-DEVICE = '/cpu:0'
-
-# Create model in inference mode
-with tf.device(DEVICE):
-    model = modellib.MaskRCNN(mode="inference",
-                              model_dir=LOGS_DIR,
-                              config=config)
-
-
-model.load_weights(weights, by_name=True)
-
-#%%
-
-def get_ax(rows=1, cols=1, size=16):
-    """Return a Matplotlib Axes array to be used in
-    all visualizations in the notebook. Provide a
-    central point to control graph sizes.
-    
-    Adjust the size attribute to control how big to render images
-    """
-    fig, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
-    fig.tight_layout()
-    return ax
-
-
-
-#%%
-
-print("imageID: {}.{} ({}) {}".format(info["source"], info["id"], image_id, 
-                                       dataset.image_reference(image_id)))
-print("Original image shape: ", modellib.parse_image_meta(image_meta[np.newaxis,...])["original_image_shape"][0])
-
-# Run object detection
-results = model.detect_molded(np.expand_dims(image, 0), np.expand_dims(image_meta, 0), verbose=1)
-
-# Display results
-r = results[0]
-# log("gt_class_id", gt_class_id)
-# log("gt_bbox", gt_bbox)
-# log("gt_mask", gt_mask)
-
-# Compute AP over range 0.5 to 0.95 and print it
-# AP stands for average precision, as detailed here: https://cocodataset.org/#detection-eval
-utils.compute_ap_range(gt_bbox, gt_class_id, gt_mask,
-                       r['rois'], r['class_ids'], r['scores'], r['masks'],
-                       verbose=1)
-
-mAP, precisions, recalls, overlaps = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
-                 r['rois'], r['class_ids'], r['scores'], r['masks'])
-
-
-visualize.display_differences(
-    image,
-    gt_bbox, gt_class_id, gt_mask,
-    r['rois'], r['class_ids'], r['scores'], r['masks'],
-    dataset.class_names, ax=get_ax(),
-    show_box=False, show_mask=False,
-    iou_threshold=0.5, score_threshold=0.5)
-
-
-
-
-#%%
-
-# compute_matches links predicted masks with GT masks
-# gt_match is an array of indices for the matched predicted mask, as related
-# to the GT mask
-# pred_match is an array of the indices linking predicted masks to a GT mask
-# compare_matches also calculates overlaps following matching. overlaps.shape
-# is [predicted_nuclei, gt_nuclei]
-gt_match, pred_match, overlaps = utils.compute_matches(
-    gt_bbox, gt_class_id, gt_mask,
-    r['rois'], r['class_ids'], r['scores'], r['masks'],
-    iou_threshold=0.5, score_threshold=0.5)
-
-# Use compute_matches to get IoU, recall, precision etc. 
-
-#%% Mask rcnn stats:
-mAP, precisions, recalls, overlaps = utils.compute_ap(gt_bbox, gt_class_id, gt_mask,
-             r['rois'], r['class_ids'], r['scores'], r['masks'],
-             iou_threshold=0.5)
-
-# Total predictions that have a GT match / total number of predictions
-# (pred_match > -1) returns T/F depending if there is a match to a GT mask.
-# This creates a bool array
-# np.cumsum then calculates the cumulative sum where true = 1 and false = 0
-# for the length of pred_match.
-#
-# (np.arange(len(pred_match)) + 1) then creates an array 
-# (+ 1 starts it from 1 rather than 0) for the length of predicted matches
-#
-# Then, by dividing these it yields a iterative calculation of precision.
-# The order of pred_match is from highest score to lowest (from compute_matches)
-# np.cumsum when encountering false will not increase, thus by being divided
-# by the steadily increasing np.arrange will lead to a value < 1.
-# eg. 3 / 3 = 1, but 2 / 3 = 0.66...
-# This solution from Mask R-CNN is beautiful and efficient
-
-np.cumsum(pred_match > -1) / (np.arange(len(pred_match)) + 1)
-
-#%% Calculating F1
-
-# For all of the predicted matches, find those that have a matched gt mask
-# and get the length of this array
-true_pos = np.where(pred_match > -1)[0].shape[0]
-
-# len(pred_match) is all of the predicted masks, positive and negative
-precision = true_pos / len(pred_match)
-
-# len(gt_match) is all of the true (+ve) and false (-ve) positives
-recall = true_pos / len(gt_match)
-
-# Calculate f1
-f1 = 2 * (precision * recall) / (precision + recall)
-
-
-def calculate_f1(gt_matches, pred_matches):
-    """
-    Calculates the F1 score given matches gt and predicted mask from Mask R-CNN output
-    """
-    
-    # For all of the predicted matches, find those that have a matched gt mask
-    # and get the length of this array
-    true_pos = np.where(pred_matches > -1)[0].shape[0]
-    
-    # len(pred_match) is all of the predicted masks, positive and negative
-    precision = true_pos / len(pred_match)
-    
-    # len(gt_match) is all of the true (+ve) and false (-ve) positives
-    recall = true_pos / len(gt_match)
-    
-    # Calculate f1
-    f1 = 2 * (precision * recall) / (precision + recall)
-    
-    return f1
-    
-    
-    
-#%% Return the IoU values
-
-for i in overlaps:
-    for j in i:
-        if j != 0:
-            print(j)
-            
-#%% Convert image dtype to uint8
-
-out = np.zeros(image.shape, dtype=np.uint8)
-
-out = cv2.normalize(image, out, 0, 255, cv2.NORM_MINMAX)
-
-# Fixed! Converting the image to uint8 is the key! The strange gray 
-# image wasn't because of RGB or anything, but instead due to the 
-# metamorph image being uint16 rather than uint8
-
-#%%
-
-
-visualize.display_differences(
-    out,
-    gt_bbox, gt_class_id, gt_mask,
-    r['rois'], r['class_ids'], r['scores'], r['masks'],
-    dataset.class_names, ax=get_ax(),
-    show_box=False, show_mask=False,
-    iou_threshold=0.5, score_threshold=0.5)
-
-#%%
 
 class CalculateStats:
     def __init__(self):
@@ -367,8 +65,8 @@ class CalculateStats:
                         
             # Rescale to 8-bit
             # Since model was trained on 8-bit DSB18 images
-            out = np.zeros(image.shape, dtype=np.uint8)
-            image = cv2.normalize(image, out, 0, 255, cv2.NORM_MINMAX)
+            # out = np.zeros(image.shape, dtype=np.uint8)
+            # image = cv2.normalize(image, out, 0, 255, cv2.NORM_MINMAX)
                         
             _gt_info = {"image": image,
                        "image_meta": image_meta,
@@ -415,9 +113,9 @@ class CalculateStats:
             
             self.gt_detection_info.append(gt_and_detection)
             
-    def calculate_matches_and_f1(self, iou):
+    def calculate_matches_and_f1(self, iou_level):
         """
-        Find matches between gt masks and predicted masks. iou determines the
+        Find matches between gt masks and predicted masks. iou_level determines the
         minimum threshold to determine a match. 
         
         Returns a list of dicts that contains image, predicted masks, gt masks,
@@ -429,19 +127,21 @@ class CalculateStats:
             gt_match, pred_match, overlaps = utils.compute_matches(
                     gt["gt_bbox"], gt["gt_class_id"], gt["gt_mask"],
                     gt['rois'], gt['class_ids'], gt['scores'], gt['masks'],
-                    iou_threshold=iou, score_threshold=0.5)
+                    iou_threshold=iou_level, score_threshold=0.5)
             
             # Add dict as an element in the gt_detection_info list
             self.gt_detection_info[i].update({"gt_match": gt_match,
                                               "pred_match": pred_match,
                                               "overlaps": overlaps,
-                                              "f1": calculate_f1(gt_match, pred_match)})
+                                              "f1": calculate_f1(gt_match, pred_match)[0],
+                                              "precision": calculate_f1(gt_match, pred_match)[1],
+                                              "recall": calculate_f1(gt_match, pred_match)[2]})
                        
 
 
 def calculate_f1(gt_matches, pred_matches):
     """
-    Calculates the F1 score given matches gt and predicted mask from Mask R-CNN output
+    Calculates the F1, precision and recall score given matches gt and predicted mask from Mask R-CNN output
     """
     
     # For all of the predicted matches, find those that have a matched gt mask
@@ -453,11 +153,11 @@ def calculate_f1(gt_matches, pred_matches):
 
     # len(gt_match) is all of the true (+ve) and false (-ve) positives
     recall = true_pos / len(gt_matches)
-    print("Precision: {}. Recall: {}".format(precision, recall))    
+        
     # Calculate f1
     f1 = 2 * (precision * recall) / (precision + recall)
     
-    return f1
+    return f1, precision, recall
             
             
         
@@ -518,11 +218,8 @@ ax.set_xticklabels(labels=x_values, rotation=45, ha="right", rotation_mode="anch
 
 
 # Dot bar plot of f1 IoU for multiple images
-# Compare low/med config
+# Compare low/med/high config
 # Zoomed in images, 
-
-
-
 
 
 # Compare missed objects between detection methods
@@ -531,8 +228,100 @@ ax.set_xticklabels(labels=x_values, rotation=45, ha="right", rotation_mode="anch
 # New stats as above, but on precision/recall?
 
 
+#%% low/med/high iou 0.7 f1 scores
+
+# setting = ["low"]
+setting = ["low", "med"]
+# img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/u2os-20x"
+img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/small_overlaps-BBBC004/test"
+# img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/BBBC006-oof-u2os/images+masks/oof-images"
+
+
+results = {"computation_setting": [],
+           "precision": [],
+           "recall": [],
+           "f1_score": [],
+           "execution_time": []}
+
+for i in setting:
+    start_time = time.time()
+    stats = CalculateStats()
+    stats.gt_detect(img_dir, i)
+    stats.calculate_matches_and_f1(iou_level=0.7)
+    
+    res = stats.gt_detection_info[0]
+
+    results["computation_setting"].append(i)
+    results["precision"].append(stats.gt_detection_info[0]["precision"])
+    results["recall"].append(stats.gt_detection_info[0]["recall"])
+    results["f1_score"].append(stats.gt_detection_info[0]["f1"])
+    results["execution_time"].append(time.time() - start_time)
+
+    fig = visualise.display_detections(res)
+    fig.savefig(i+" masks for small overlaps.png", dpi=300, bbox_inches='tight')
+    
+#%%
+# Colours for plotting. From Solarized palette
+colour_palette = [(42, 161, 152), (38, 139, 210), (108, 113, 196), (211, 54, 130)]
+plot_colours = colour_palette
+for i in range(len(colour_palette)):
+	r, g, b = colour_palette[i]
+	# Convert RGB (0, 255) to (0, 1) which matplotlib likes
+	plot_colours[i] = (r / 255, g / 255, b / 255)
+    
+#%% IoU threshold F1 scores for small overlaps
+
+
+# img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/u2os-20x"
+img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/small_overlaps-BBBC004/test"
+# img_dir = "/home/think/Documents/deep-click/datasets/nucleus/stats_images/BBBC006-oof-u2os/images+masks/oof-images"
+
+# Extract f1 scores for images over a range of IoU thresholds
+# 0.1 to 0.9
+iou_levels = np.round(np.arange(0.5, 0.95, 0.05), 2)
+
+
+
+f1_scores = {"setting": []}
+f1_scores.update({k: [] for k in iou_levels})
+
+
+setting = ["low", "med"]
+for i in setting:
+    f1_scores["setting"].append(i)
+    stats = CalculateStats()
+    stats.gt_detect(img_dir, i)
+
+    for iou_threshold in iou_levels:
+        stats.calculate_matches_and_f1(iou_level=iou_threshold)
+        
+        for i, detection in enumerate(stats.gt_detection_info):
+            f1_scores[iou_threshold].append(detection['f1'])
+        
+
+
 #%%
 
+# f1 score vs IoU
+# Compare low/med config
+# i[1] to extract the second image f1_scores
+y1 = [i[0] for i in list(f1_scores.values())[1:]]
+y2 = [i[1] for i in list(f1_scores.values())[1:]]
+# [1:] to ignore the first key, which is setting
+x_values = list(f1_scores.keys())[1:]
+
+fig, ax = plt.subplots(1, 1, figsize=(6, 3))
+ax.plot(x_values, y1, marker="o", color=plot_colours[3])
+ax.plot(x_values, y2, marker="o", color=plot_colours[1])
+ax.spines["top"].set_visible(False)
+ax.spines["right"].set_visible(False)
+ax.set_xlabel("IoU Threshold")
+ax.set_ylabel("F1 Score")
+ax.legend(["low", "med"])
+fig.tight_layout()
+fig.savefig("low vs med for small overlaps.png", dpi=300, bbox_inches='tight')
+    
+    
 
     
     
