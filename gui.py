0f5a46dcc309344714648c0584feae83d4ee1e6b
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2021-03-25 18:50:00 +0000

    added opened image rescaling and also trying to work out how I can iteratively update a widget from another widget

--- gui.py
+++ gui.py
@@ -108,11 +108,14 @@ btn_browse.grid(row = 0, column = 1)
 # https://dev.to/abdurrahmaanj/building-an-oop-calculator-and-what-it-means-to-write-a-widget-library-4560
 
 import tkinter as tk
+from tkinter import filedialog
 import os
 from PIL import Image
 from PIL import ImageTk
+from threading import Thread
+# https://www.pythontutorial.net/tkinter/tkinter-thread/
 
-
+import time
 
 class BrowseFiles:
     """
@@ -128,7 +131,7 @@ class BrowseFiles:
         # contained within. 
         self.container.grid(row = row, column = column)
         
-        def browsefiles():
+        def browse_files():
             # Prompt user to select birectory
             self.filename = filedialog.askdirectory()
             # Display selected folder
@@ -143,21 +146,19 @@ class BrowseFiles:
         
         def get_image_selection(_selection):
             img_selection = self.dir_contents.curselection()
-            img_filename = self.dir_contents.get(img_selection)
-
-            
-            img = tk.PhotoImage(file = img_filename)
-            
+            self.img_filename = self.dir_contents.get(img_selection)
+            img_open = Image.open(self.img_filename)
+            width, height = img_open.size
+            aspect = width / height
+            # Round leads to approximation of the height, but it should
+            # not distort the image too much, hopefully...
+            img_resized = img_open.resize((500, round(500/aspect)))      
+            img = ImageTk.PhotoImage(img_resized)
             new_window = tk.Toplevel()
-            new_window.title(img_filename)
-            
+            new_window.title(self.img_filename)   
             lab = tk.Label(new_window, image = img)
             lab.grid(row = 0, column = 0)
-
-            
             new_window.mainloop()
-            
-            #print(img)
             
         # Textbox to display selected directory
         self.lbl_explorer = tk.Label(
@@ -172,7 +173,7 @@ class BrowseFiles:
             master = self.container, 
             width = 20, 
             text = "Browse",
-            command = browsefiles)
+            command = browse_files)
         
         # List box to display the image files found in the selected dir
         self.dir_contents = tk.Listbox(
@@ -186,16 +187,77 @@ class BrowseFiles:
         self.btn_browse.grid(row = 0, column = 1)
         self.dir_contents.grid(row = 1, column = 0)
         
-class ImageBox:
+class Iterator:
+    """
+    For testing passing data between widgets/classes
+    """
     def __init__(self, parent, row, column):
         self.parent = parent
         self.container = tk.Frame(self.parent)
         self.container.grid(row = row, column = column)
         
-        
-        
+        self.list = []
 
+        def increase():
+            value = int(lbl_value["text"])
+            lbl_value["text"] = value + 1
+            app.print_iterator(int(lbl_value["text"]))
+            # Trying to work out how to detect a list has changed between classes
+            self.list.append(value)
 
+        def decrease():
+            value = int(lbl_value["text"])
+            lbl_value["text"] = value - 1
+            app.print_iterator(int(lbl_value["text"]))
+            
+        def trigger():
+            l = ListIterator()
+            app.print_iterator(l.list)
+
+        btn_decrease = tk.Button(self.container, text="-", command=decrease)
+        btn_decrease.grid(row=0, column=0, sticky="nsew")
+        
+        lbl_value = tk.Label(self.container, text="0")
+        lbl_value.grid(row=0, column=1)
+        
+        btn_increase = tk.Button(self.container, text="+", command=increase)
+        btn_increase.grid(row=0, column=2, sticky="nsew")    
+
+        btn_trigger = tk.Button(self.container, text = "!!!", command = trigger)
+        btn_trigger.grid(row=1, column=1, sticky="nsew") 
+
+        
+class ListIterator:
+    """
+    Class to mimic the detection of deepQIBC. 
+    
+    I'm trying to work out if I can display the detections as they 
+    are measured.
+    
+    Therefore, I want to pass self.list 5 times to the master window, 
+    rather than just passing the completed list.
+    
+    Should I run it in a different thread? I think so
+    """
+    def __init__(self):
+        self.list = []
+
+        for i in range(5):
+            time.sleep(0.5)
+            self.list.append(i)
+            print(self.list)
+        
+class IteratorDisplay:
+    def __init__(self, parent, row, column, data):
+        self.parent = parent
+        self.container = tk.Frame(self.parent)
+        self.container.grid(row = row, column = column)
+        
+        lbl_display = tk.Label(self.container, text = str(data))
+        lbl_display.grid(row=0, column=0)
+        
+    
+        
 class DeepQIBCgui(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         self.master = master
@@ -204,19 +266,18 @@ class DeepQIBCgui(tk.Frame):
         
         browse1 = BrowseFiles(self.master, 0, 1)
 
+        iterator = Iterator(self.master, 1, 1)
         
+        
+    def print_iterator(self, data):
+        disp = IteratorDisplay(self.master, 1, 2, data)
 
 if __name__ == "__main__":
     root = tk.Tk()
-    DeepQIBCgui(root)
+    app = DeepQIBCgui(root)
     root.mainloop()
 
 
-
-
-
-
-window.mainloop()
 
 
 
