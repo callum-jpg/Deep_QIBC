1a3654b92ca67b7ebe957021777cec701fc96758
callum-jpg <23258116+callum-jpg@users.noreply.github.com>
2021-03-29 19:03:05 +0100

    trying to use threading. concept works, need to implement into detect

--- gui.py
+++ gui.py
@@ -113,7 +113,7 @@ import os
 from PIL import Image
 from PIL import ImageTk
 from threading import Thread
-# https://www.pythontutorial.net/tkinter/tkinter-thread/
+import queue
 
 import time
 
@@ -196,38 +196,61 @@ class Iterator:
         self.container = tk.Frame(self.parent)
         self.container.grid(row = row, column = column)
         
-        self.list = []
-
-        def increase():
-            value = int(lbl_value["text"])
-            lbl_value["text"] = value + 1
-            app.print_iterator(int(lbl_value["text"]))
-            # Trying to work out how to detect a list has changed between classes
-            self.list.append(value)
-
-        def decrease():
-            value = int(lbl_value["text"])
-            lbl_value["text"] = value - 1
-            app.print_iterator(int(lbl_value["text"]))
-            
-        def trigger():
-            l = ListIterator()
-            app.print_iterator(l.list)
-
-        btn_decrease = tk.Button(self.container, text="-", command=decrease)
+        btn_decrease = tk.Button(self.container, text="-", command=self.decrease)
         btn_decrease.grid(row=0, column=0, sticky="nsew")
         
-        lbl_value = tk.Label(self.container, text="0")
-        lbl_value.grid(row=0, column=1)
+        self.lbl_value = tk.Label(self.container, text="0")
+        self.lbl_value.grid(row=0, column=1)
         
-        btn_increase = tk.Button(self.container, text="+", command=increase)
+        btn_increase = tk.Button(self.container, text="+", command=self.increase)
         btn_increase.grid(row=0, column=2, sticky="nsew")    
 
-        btn_trigger = tk.Button(self.container, text = "!!!", command = trigger)
+        btn_trigger = tk.Button(self.container, text = "!!!", command = self.trigger)
         btn_trigger.grid(row=1, column=1, sticky="nsew") 
+        
+        self.list = []
+
+    def increase(self):
+        value = int(self.lbl_value["text"])
+        self.lbl_value["text"] = value + 1
+        app.print_iterator(int(self.lbl_value["text"]))
+        # Trying to work out how to detect a list has changed between classes
+
+    def decrease(self): 
+        value = int(lbl_value["text"])
+        self.lbl_value["text"] = value - 1
+        app.print_iterator(int(self.lbl_value["text"]))
+        
+    def trigger(self):
+        #l = ListIterator()
+        self.queue = queue.Queue()
+        ListIterator(self.queue).start()
+        
+        self.parent.after(100, self.process_queue)
+
 
         
-class ListIterator:
+    def process_queue(self):
+        try:
+            msg = self.queue.get(0)
+            print(msg)
+            # Show result of the task if needed
+            self.parent.after(100, self.process_queue)
+            app.print_iterator1(msg)
+        except queue.Empty:
+            self.parent.after(100, self.process_queue)
+
+class IteratorDisplay:
+    def __init__(self, parent, row, column, data):
+        self.parent = parent
+        self.container = tk.Frame(self.parent)
+        self.container.grid(row = row, column = column)
+        
+        lbl_display = tk.Label(self.container, text = str(data))
+        lbl_display.grid(row=0, column=0)   
+     
+
+class ListIterator(Thread):
     """
     Class to mimic the detection of deepQIBC. 
     
@@ -239,25 +262,19 @@ class ListIterator:
     
     Should I run it in a different thread? I think so
     """
-    def __init__(self):
-        self.list = []
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
 
+    # class method called "run" to enable threading
+    def run(self):
         for i in range(5):
             time.sleep(0.5)
-            self.list.append(i)
-            print(self.list)
-        
-class IteratorDisplay:
-    def __init__(self, parent, row, column, data):
-        self.parent = parent
-        self.container = tk.Frame(self.parent)
-        self.container.grid(row = row, column = column)
-        
-        lbl_display = tk.Label(self.container, text = str(data))
-        lbl_display.grid(row=0, column=0)
-        
-    
-        
+            self.queue.put(i)
+
+
+
+
 class DeepQIBCgui(tk.Frame):
     def __init__(self, master, *args, **kwargs):
         self.master = master
@@ -265,6 +282,8 @@ class DeepQIBCgui(tk.Frame):
         browse = BrowseFiles(self.master, 0, 0)
         
         browse1 = BrowseFiles(self.master, 0, 1)
+        
+        iterator = Iterator(self.master, 1, 0)
 
         iterator = Iterator(self.master, 1, 1)
         
@@ -272,37 +291,81 @@ class DeepQIBCgui(tk.Frame):
     def print_iterator(self, data):
         disp = IteratorDisplay(self.master, 1, 2, data)
 
+    def print_iterator1(self, data):
+        disp = IteratorDisplay(self.master, 2, 1, data)
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = DeepQIBCgui(root)
     root.mainloop()
 
 
+#%% Testing others code
+
+import queue
+from tkinter import ttk
+
+class GUI:
+    def __init__(self, master):
+        self.master = master
+        self.test_button = tk.Button(self.master, command=self.tb_click)
+        self.test_button.configure(
+            text="Start", background="Grey",
+            padx=50
+            )
+        self.test_button.pack(side=tk.TOP)
+        
+        self.test_button1 = tk.Button(self.master)
+        self.test_button1.configure(
+            text="it does nothing", background="Grey",
+            padx=50
+            )
+        self.test_button1.pack(side=tk.BOTTOM)
 
 
+    def progress(self):
+        self.prog_bar = ttk.Progressbar(
+            self.master, orient="horizontal",
+            length=200, mode="indeterminate"
+            )
+        self.prog_bar.pack(side=tk.TOP)
 
+    def tb_click(self):
+        self.progress()
+        self.prog_bar.start()
+        self.queue = queue.Queue()
+        ThreadedTask(self.queue).start()
+        self.master.after(100, self.process_queue)
 
+    def process_queue(self):
+        try:
+            # This is entered when the queue is not empty. 
+            # Here, message is the "Task finished" message
+            msg = self.queue.get(0)
+            print("queue get", msg)
+            self.master.after(100, self.process_queue)
+            #self.prog_bar.stop()
+        except queue.Empty:
+            # If the queue is empty, test it again after 100 seconds
+            self.master.after(100, self.process_queue)
 
+class ThreadedTask(Thread):
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+    def run(self):
+        #time.sleep(2)
+        for i in range(5):
+            time.sleep(1)
+            self.queue.put(i)
+        # Put an item into the quene
+        # This is executed after the sleep function has run
+        #self.queue.put("Task finished") 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+root = tk.Tk()
+root.title("Test Button")
+main_ui = GUI(root)
+root.mainloop()
 
 
 
